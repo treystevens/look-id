0de4aeb86b4a3a86735a760e248bc4ffdb9aad9d
treykris <trey10027@gmail.com>
2018-06-28 18:49:17 -0400

    Clear inputs on submit, incorrect info message

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,41 +1,36 @@
 import React, { Component } from 'react';
 import '../styles/styles.css';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { addAuth } from '../actions/addAuth';
+import { Redirect } from 'react-router';
+
+
 
 class Login extends Component{
 
     constructor(props){
-        super(props)
+        super(props);
 
         this.state = {
             username: '',
             password: '',
-            errors: {},
             errorStatus: false
-        }
+        };
         
         this.submitHandler = this.submitHandler.bind(this);
         this.usernameChange = this.usernameChange.bind(this);
         this.passwordChange = this.passwordChange.bind(this);
+        this.clearFields = this.clearFields.bind(this);
 
     }
 
-    componentDidMount(){
-        fetch('/auth/login',{
-            method: 'GET',
-            credentials: 'include'
-        })
-        .then((res) => {
-            console.log(res)
-            return res.json()
-        })
-        .then((user) => {
-            console.log(user, `app fetch to /login`)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+    clearFields(){
+        let fields = document.querySelectorAll('.userfield');
+        for(let i of fields){
+            i.value = '';
         }
+    }
     
     usernameChange(evt){
         this.setState({username: evt.target.value});
@@ -47,6 +42,8 @@ class Login extends Component{
 
     submitHandler(evt){
         evt.preventDefault();
+        this.clearFields();
+        
         
         const data = {
             username: this.state.username,
@@ -63,19 +60,23 @@ class Login extends Component{
         })
         .then((res) => {
             // console.log(res);
+
+            if(res.status === 401){
+                this.setState({
+                    errorStatus: true
+                })
+                return 1;
+            }
+
             return res.json();
         })
         .then((logged) => {
             
-            if(logged.success){
-                console.log(logged.user)
+            if(logged.actionSuccess){
+                console.log(logged.user);
                
 
-                this.props.dispatch({
-                    type: 'AUTHENTICATE', 
-                    user: logged.user.username,
-                    userID: logged.user.userID
-                });
+                this.props.dispatch(addAuth(logged.user));
                 
             }
 
@@ -86,22 +87,31 @@ class Login extends Component{
     }
 
     render(){
+
+        if(this.props.isAuth){
+            return <Redirect to='/'/>
+        }
+
+
         return(
             <div className="container  img-spread1" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
                 <div className="flex-container" style={{width: '60%', height: '70vh', backgroundColor: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
                     <form className="test-form" autoComplete="off" onSubmit={this.submitHandler} style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-                    
+
+                    {this.state.errorStatus &&
+                    <span>Username or password is incorrect</span>}
                         <label>Username:
-                            <input type="text" placeholder="Username" name="username" onChange={this.usernameChange}required/>
+                            <input type="text" placeholder="Username" name="username" onChange={this.usernameChange}required  className="userfield"/>
                         </label>
 
                     
                         <label>Password:
-                            <input type="password" placeholder="Password" name="password" onChange={this.passwordChange} required />
+                            <input type="password" placeholder="Password" name="password" onChange={this.passwordChange} required className="userfield"/>
                         </label>
                         
                         <button>Login</button>
                     </form>
+                    <Link to='/signup'>Don't have an account?</Link>
                 </div>
             </div>
         )

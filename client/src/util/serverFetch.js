ebbbb0582af43373e59f42d55d50f7bc5f486fb7
treykris <trey10027@gmail.com>
2018-06-30 17:26:36 -0400

    helper fetch functions

---
+++ client/src/util/serverFetch.js
@@ -0,0 +1,37 @@
+function sendUserData(url, data){
+    let userFetch = fetch(url, {
+        body: JSON.stringify(data),
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        credentials: 'include'
+    })
+    .then((res) => {
+        console.log(res);
+        return res;
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+    return userFetch;
+}
+
+function getData(url){
+    let userFetch = fetch(url, {
+        method: 'GET',
+        credentials: 'include'
+    })
+    .then((res) => {
+        console.log(res);
+        return res;
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+    return userFetch;
+}
+
+export { sendUserData, getData };

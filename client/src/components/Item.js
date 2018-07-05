069384d552bbd1bea3bff808ded518cc1f9fcfa4
treykris <trey10027@gmail.com>
2018-07-05 18:17:04 -0400

    Added new component to handle adding items

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,49 +1,159 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react';   
+import Stores from './Stores';
+import AddItem from './AddItem';
+
+/* jshint ignore:start */
 
 class Item extends Component{
     constructor(props){
         super(props);
 
+        this.state = {
+            items: [{
+                name: '',
+                category: '',
+                color: '',
+                price: '',
+                thrifted: false,
+                stores: [{
+                    store: ''
+                }],
+                link: ''
+            }]
+        };
 
-        this.handleDelete = this.handleDelete.bind(this)
+
+    }
+
+    // Function that will handle all of our input state
+    handleInputChange = (index, propertyName) => (evt) => {
+        const newItems = this.state.items.map((item, itemIndex) => {
+            if( index !== itemIndex) return item;
+            return { ...item, [propertyName]: evt.target.value };
+        })
+
+        this.setState({ items: newItems}, () => {
+            this.props.addItemToParentState(this.state.items)
+        })
+    };
+
+    // Handle the Thrifted Checkbox state
+    handleThriftedChange = (index) => (evt) => { 
+        console.log(evt.target.checked);
+        const newItems = this.state.items.map((item, itemIndex) => {
+            if( index !== itemIndex) return item;
+            return { ...item, thrifted: evt.target.checked };
+        })
+
+        this.setState({ items: newItems}, () => {
+            console.log(this.state.items)
+
+            this.props.addItemToParentState(this.state.items)
+        })
+    }   
+    
+    // Adding new item
+    handleAddItem = () => {
+
+        if(this.state.items.length > 5){
+            return 1;
+        }
+
+        this.setState({ items: this.state.items.concat([{ 
+            name: '',
+            category: '',
+            color: '',
+            price: '',
+            thrifted: false,
+            stores: [{
+            store: ''
+                        }],
+            link: '' }]) 
+    });
     }
 
 
-    handleDelete(evt){
-        console.log(evt.target);
+    // Remove Item
+    handleRemoveItem = (index) => () => {
+        this.setState({ 
+            items: this.state.items.filter( (item, itemIndex) => index !== itemIndex) 
+        });
+    };
 
+    // Lifting state from Store Component and adding to this state
+    handleAddStoreToState = (storeState, index) => {
+        const newItems = this.state.items.map((item, itemIndex) => {
+            if( index !== itemIndex) return item;
+            return { ...item, stores: storeState };
+        })
 
-        this.props.deleteItem(this.props.itemNumber);
+        this.setState({ items: newItems}, () => {
+            console.log(this.state.items)
+        })
     }
+
 
     render(){
         return(
-                <div style={{display: 'flex', flexFlow: 'row wrap', position: 'relative', width: '30%'}}>
-                    <div style={{position: 'absolute', right: '5px', top: '5px'}} onClick={this.handleDelete}>Close</div>
-                    <label>Item Name:
-                        <input type="text" name='itemname' />
-                    </label>
-                    <label>Category:
-                        <input type="text" name='itemcategory' />
-                    </label>
-                    <label>Color:
-                        <input type="text" name='itemcolor' />
-                    </label>
-                    <div style={{display: 'flex'}}>
-                        <label>$
-                            <input type="text" name='itemprice'/>
-                        </label>
-                        <label>Thrifted
-                            <input type="checkbox" name='itemthrifted'/>
-                        </label>
+                
+            <section>
+                {this.state.items.map((item, index) => (
+                    <div style={{display: 'flex', flexFlow: 'row wrap', position: 'relative', width: '30%', marginTop: '40px'}}>
+                        <div>
+                            <label>Name:
+                                <input type="text" placeholder={`Item #${index + 1} name`} value={item.name}
+                                onChange={this.handleInputChange(index, `name`)} />
+                            </label>
+                        </div>
+
+                        <div>
+                            <label>Category:
+                                <input type="text" placeholder={`Item #${index + 1} category`} value={item.category}
+                                onChange={this.handleInputChange(index, `category`)}/>
+                            </label>
+                        </div>
+
+                        <div>
+                            <label>Color:
+                                <input type="text" placeholder={`Item #${index + 1} color`} value={item.color}
+                                onChange={this.handleInputChange(index, `color`)}/>
+                            </label>
+                        </div>
+
+                        <div>
+                            <label>Price:
+                                <input type="text" placeholder={`Item #${index + 1} price`} value={item.price}
+                                onChange={this.handleInputChange(index, `price`)} />
+                            </label>
+                            <label>Thrifted:
+                                <input type="checkbox" onClick={this.handleThriftedChange(index)} />
+                            </label>
+                        </div>
+
+                        <div>
+                            <label>Store:
+                                <Stores index={index} handleStore={this.handleAddStoreToState}/>
+                            
+                                {/* <button type="button" onClick={this.handleAddStore} className="small">Add Store</button> */}
+                            </label>
+                        </div>
+
+                        <div>
+                            <label>Online Link:
+                                <input type="text" placeholder={`Item #${index + 1} online link`} value={item.link}
+                                onChange={this.handleInputChange(index, `link`)}  />
+                            </label>
+                        </div>
+
+                        <button type="button" onClick={this.handleRemoveItem(index)}>X</button>
+                    
+                        
                     </div>
-                    <label>Store
-                        <input type="text" name='itemstore'/>
-                    </label>
-                    <label>Online Link
-                        <input type="text" name='itemlink'/>
-                    </label>
-                </div>
+                        
+                ))}
+
+                <AddItem handleAddItem={this.handleAddItem} />
+            </section>
         )
     }
 }
@@ -51,4 +161,11 @@ class Item extends Component{
 
 
 export default Item;
+
+/* jshint ignore:end */
+
+
+
+
+
 

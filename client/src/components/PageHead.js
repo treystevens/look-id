1cbf8f37f4773a7a23ae7bb77c98b1843f4f4d7b
treykris <trey10027@gmail.com>
2018-07-05 18:20:43 -0400

    Added profile name to visiting user profile

--- client/src/components/PageHead.js
+++ client/src/components/PageHead.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 const PageHead = (props) => {
+
+    let postPage;
+    let profileLink;
+
+    if(props.post){
+        postPage = props.post;
+        profileLink = `/user/${props.pageHead}`;
+    }
+    
+    
+
     return(
         <section>
-            <h1>{props.pageHead}</h1>
+            {postPage ? (
+                <Link to={profileLink}><h1>{props.pageHead}</h1></Link>
+                ) : (
+                <h1>{props.pageHead}</h1>
+                )
+            }
         </section>
     )
 }

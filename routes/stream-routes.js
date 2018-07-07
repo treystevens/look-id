864f0f8808d279b91cb031ef4f3bba043a09cb28
treykris <trey10027@gmail.com>
2018-07-07 17:54:49 -0400

    stream route added for explore and feed

---
+++ routes/stream-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const TestExplore = require('../models/testexplore');
+
+
+
+router.get('/explore', (req, res) => {
+    
+    TestExplore.find({})
+    .then((response) => {
+        console.log(response);
+        console.log(response[0].stream);
+
+        res.json({stream: response[1].stream});
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+});
+
+module.exports = router;

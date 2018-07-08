54d4816909571ca167980eaf2b00a62c23a4d77d
treykris <trey10027@gmail.com>
2018-07-08 02:48:38 -0400

    Properly add and delete comment from database

--- routes/comment-routes.js
+++ routes/comment-routes.js
@@ -24,20 +24,33 @@ router.post('/', (req, res) => {
 
     // Do validation to make sure the a submit cannot be made if the comment box is empty
 
-
     const query = {
         "posts.post_id": req.body.userPage.postID
     }; 
 
-    console.log(req.body, `this is the request body`);
-    // res.json({success: true})
  
-    // Update the items for that specific post
-
-    AccountInfo.findOneAndUpdate(query, {$push: {"posts.$.comments": req.body.newComment}}, {new: true})
+    AccountInfo.findOneAndUpdate(query, {$push: {"posts.$.comments": req.body.newComment}}, {
+        "new": true
+       })
     .then( (data) => {
+        AccountInfo.findOne(
+            query, {"posts.$": 1})
+            .then((data) => {
+                console.log(data);
 
-        res.json({success: true, comment: req.body.newComment});
+                const databaseComments = data.posts[0].comments;
+                const length = data.posts[0].comments.length;
+
+                const newComment = databaseComments[length-1];
+
+                res.json({success: true, comment: newComment});
+
+                
+            });
+
+    
+
+        
         
     })
     .catch( (err) => {
@@ -45,6 +58,22 @@ router.post('/', (req, res) => {
     });
 
 });
+
+
+router.post('/delete', (req, res) => {
+    
+    const query = { "posts.comments._id": req.body.id}; 
+    
+    AccountInfo.update(query, { $pull: { 'posts.$.comments': {
+        "_id":req.body.id}}})
+    .then( () => {
+        res.json({success: true});
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+})
 
 
 module.exports = router;

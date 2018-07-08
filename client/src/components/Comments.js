b7f59cd3fd14720bcdf097bbd82a24d73e31038d
treykris <trey10027@gmail.com>
2018-07-08 11:19:41 -0400

    view all and collapse comment functionality

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -3,7 +3,6 @@ import CommentBox from './CommentBox';
 import CommentRow from './CommentRow';
 import { getData, sendUserData } from '../util/serverFetch';
 import { connect } from 'react-redux';
-import { SSL_OP_CIPHER_SERVER_PREFERENCE } from 'constants';
 // import Notifications from './Notifications';
 
 
@@ -13,12 +12,87 @@ class Comments extends Component{
         super(props);
         this.state = {
             comments: [],
-            shownComments: []
+            shownComments: [],
+            commentsViewAll: false
             
         };
 
         this.handleAddComment = this.handleAddComment.bind(this);
         this.handleDeleteComment = this.handleDeleteComment.bind(this);
+        this.handleClickViewAll = this.handleClickViewAll.bind(this);
+        this.getInitialComments = this.getInitialComments.bind(this);
+        this.showAllComments = this.showAllComments.bind(this);
+    }
+
+    getInitialComments(){
+        const urlPageUsername = this.props.urlParams.username;
+        const urlPagePostID = this.props.urlParams.postID;
+
+        const serverResponse = getData(`/comment/${urlPageUsername}/${urlPagePostID}`);
+        
+        serverResponse.then( response => response.json())
+        .then((data) => {
+            console.log(data);
+
+
+            let viewingUser = this.props.username || false;
+
+
+            let userComments = data.comments.map((comment) => {
+                return <CommentRow comment={comment} handleDeleteComment={this.handleDeleteComment} urlParams={this.props.urlParams} viewingUser={viewingUser} key={comment._id} />
+            })
+    
+    
+            this.setState({
+                comments: userComments
+            })
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
+    showAllComments(){
+        const urlPageUsername = this.props.urlParams.username;
+        const urlPagePostID = this.props.urlParams.postID;
+
+        const serverResponse = getData(`/comment/${urlPageUsername}/${urlPagePostID}/viewall`);
+        
+        serverResponse.then( response => response.json())
+        .then((data) => {
+            console.log(data);
+
+            let viewingUser = this.props.username || false;
+
+            let userComments = data.comments.map((comment) => {
+                return <CommentRow comment={comment} handleDeleteComment={this.handleDeleteComment} urlParams={this.props.urlParams} viewingUser={viewingUser} key={comment._id} />
+            })
+    
+    
+            this.setState({
+                comments: userComments
+            })
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
+    handleClickViewAll(evt) {
+        evt.preventDefault();
+
+        let view;
+
+        // Mimic toggle
+        this.state.commentsViewAll ? view = false : view = true
+
+
+        this.setState({
+            commentsViewAll: view
+        }, () => {
+            this.state.commentsViewAll ? this.showAllComments() : this.getInitialComments();
+        })
+    
     }
 
 
@@ -69,77 +143,32 @@ class Comments extends Component{
         
     }
 
-    // Component will mount might need to get some authentication from the server if I'm trying to post a comment
-
-
     componentDidMount(){
         // let sendToCommentBox = {
         //     comment_amount: user1.comments.amount,
         //     comment_messages: user1.comments.messages,
         // }
 
-        const urlPageUsername = this.props.urlParams.username;
-        const urlPagePostID = this.props.urlParams.postID;
-
-        const serverResponse = getData(`/comment/${urlPageUsername}/${urlPagePostID}`);
+        this.getInitialComments();
         
-        serverResponse.then( response => response.json())
-        .then((data) => {
-            console.log(data);
-
-
-            let viewingUser = this.props.username || false;
-
-
-
-
-            // Store the user from the redux of the current user -> Pass the user into comment row -> Check if the comment.user from map inside CommentRow component matches the imUser variable here that is from the redux -> 
-
-            let userComments = data.comments.map((comment) => {
-                return <CommentRow comment={comment} handleDeleteComment={this.handleDeleteComment} urlParams={this.props.urlParams} viewingUser={viewingUser} key={comment._id} />
-            })
-    
-    
-            this.setState({
-                comments: userComments
-            })
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-
-        // When this component mounts we just want to take the comments from the POST componenet because we will be passing in the props from there
-        console.log(this.props);
-
-        let comments = this.state.comments;
-
-        // Structure of comment
-        // let addedNewComment = {
-        //     username: this.props.username, // get from auth token I guess...from cookie?
-        //     postID: '0201',
-        //     date_posted: date,
-        //     comment: newComment,
-        //     avatar: '/lookid/fit6.jpg'
-        // };
-
-
-
-
         
     }
 
 
     render(){
-        console.log(this.state)
+
+        let commentViewAction;
+
+        this.state.commentsViewAll ? commentViewAction = 'Collapse comments' : commentViewAction = 'View all comments'
+
         return(
             <div>
                 <CommentBox handleAddComment={this.handleAddComment} urlParams={this.props.urlParams}/>
-                {/* {this.state.shownComments} */}
                 <div>
                     <div>
                         {this.state.comments}
                     </div>
-                    <a href="/viewallcomments">View all comments</a>
+                    <a href="/viewallcomments" onClick={this.handleClickViewAll}>{commentViewAction}</a>
                 </div> 
             </div>
             

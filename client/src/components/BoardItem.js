37bf23610984ef1229fed90ada993dea5a3d20d5
treykris <trey10027@gmail.com>
2018-07-09 16:06:31 -0400

    Conditional rendering if adding a post to board or just viewing boards

--- client/src/components/BoardItem.js
+++ client/src/components/BoardItem.js
@@ -1,28 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 
 const BoardItem = (props) => {
-
-
-
     console.log(props)
 
-    let boardLink = `/user/${props.boardInfo.user}/boards/${props.boardInfo.board_id}`
-    let backgroundImg = props.boardInfo.board_display_image;
+    let boardLink = `/user/${props.username}/boards/${props.board.board_id}`;
+    let backgroundImg = props.board.display_image;
+
+
+
+    
 
     return(
         
         
          <div className="board">
-            <Link to={boardLink}>
-                <div className="boardImg" style={ { backgroundImage:`url(${backgroundImg}`} }data-board-id={props.boardInfo.board_id}></div>
-                <h1>{props.boardInfo.board_name}</h1>
-            </Link>
+             {props.addToBoard ? (
+                 <div onClick={props.handleClickBoard}>
+                    <div className="boardImg" style={ { backgroundImage:`url(${backgroundImg}`, backgroundColor: 'gray'} } data-board-id={props.board.board_id}></div>
+                    <h1>{props.board.name}</h1>
+                </div>
+                ) : (
+                <Link to={boardLink}>
+                    <div className="boardImg" style={ { backgroundImage:`url(${backgroundImg}`, backgroundColor: 'gray'} }data-board-id={props.board.board_id}></div>
+                    <h1>{props.board.name}</h1>
+                </Link>
+                )}
+
         </div>
     )
 }
 
 
+function mapStateToProps(state){
+        return {
+            username: state.username
+        }   
 
-export default BoardItem;
+}
+
+
+export default connect(mapStateToProps)(BoardItem);

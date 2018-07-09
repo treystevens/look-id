47d1be1c6e674ef90be15ffc55d5e59601616254
treykris <trey10027@gmail.com>
2018-07-09 16:08:01 -0400

    Transformed into stateful component to pass state to parent

--- client/src/components/BoardModal.js
+++ client/src/components/BoardModal.js
@@ -1,23 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Modal from './Modal';
 import { connect } from 'react-redux';
 
 
-const BoardModal = (props) => {
+class BoardModal extends Component {
+    constructor(props){
+        super(props);
 
+        this.state = {
+            boardName: ''
+        };
+
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleBoardNameChange = this.handleBoardNameChange.bind(this);
+    }
+
+
+    handleBoardNameChange(evt){
+        this.setState({
+            boardName: evt.target.value
+        });
+    }
+
+    handleSubmit(evt){
+        evt.preventDefault();
+
+        this.props.handleNewBoardSubmit(this.state.boardName);
+
+        this.setState({
+            boardName: ''
+        });
+
+        
+
+    }
+
+    render(){
         return(
             <div>
-                <form className="create-board">
-                    <label>Board Name:
-                        <input type="text" name="new-board" />
-                    </label>
-                    <input type="button" value="Cancel" />
-                    <button>Create Board</button>
-                </form>
-                {!props.isAuth && 
+                <div className="modal" onClick={this.props.closeModal}>
+                    <div className="modal-content">
+                        <form className="create-board" onSubmit={this.handleSubmit}>
+                            <label>Board Name:
+                                <input type="text" name="new-board" onChange={this.handleBoardNameChange} value={this.state.value}/>
+                            </label>
+                            <input type="button" value="Cancel" />
+                            <button>Create Board</button>
+                        </form>
+                    </div>
+                </div>
+                {!this.props.isAuth && 
                     <Modal source='accountVerify' />}
             </div>
         )
+
+    }
+        
+        
 }
 
 function mapStateToProps(state){

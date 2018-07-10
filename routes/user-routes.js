b78e8f735adaf23b64519ff4e57d4f04c853541c
treykris <trey10027@gmail.com>
2018-07-10 11:33:16 -0400

    Added to other posts in database

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -6,7 +6,6 @@ const User = require('../models/user');
 router.get('/:user', (req, res) => {
     // Display Pictures, followers, bio, avatar main profile page
 
-   
 
     const query = {
        username: req.params.user
@@ -16,7 +15,9 @@ router.get('/:user', (req, res) => {
    AccountInfo.findOne(query)
    .then( (user) => {
 
-       console.log(user)
+        if(!user) res.status(404).send('Page does not exist');
+
+    //    console.log(user);
 
        const neededData = {
            followerCount: user.followers.length,
@@ -37,35 +38,70 @@ router.get('/:user', (req, res) => {
 });
 
 
-router.get('/:user/header', (req, res) => {
-     // Display Pictures, followers, bio, avatar main profile page
+router.get('/:user/:postid', (req, res) => {
 
-    
+    const query = {
+       username: req.params.user
+   };
+    const postID = req.params.postid;
 
-     const query = {
-        username: req.params.user
+
+   AccountInfo.findOne(query)
+   .then( (user) => {
+
+    if(!user) res.status(404).send('Page does not exist');
+
+
+       let neededPostData;
+
+       user.posts.forEach((post) => {
+        if(post.post_id === postID){
+            if(!post.post_id) res.status(404).send('Page does not exist');
+            neededPostData = post;
+        }
+       });
+
+       console.log(neededPostData, ` the needed post data`);
+
+
+        const username = user.username;
+
+       res.json({post: neededPostData, username: username, myUsername: req.user.username, otherPosts: user.other_posts});
+   })
+   .catch( (err) => {
+       console.log(err);
+   });
+
+});
+
+router.post('/:postid/like', (req, res) => {
+
+    const liked = req.body.liked;
+    let performUpdateAction;
+
+    liked ? 
+            performUpdateAction = { $pull: {'posts.$.liked': req.user.username} } 
+            : 
+            performUpdateAction = { $push: {'posts.$.liked': req.user.username} }
+
+    const query = {
+        'posts.post_id': req.params.postid
     };
-    
 
-    AccountInfo.findOne(query)
-    .then( (user) => {
-
-        console.log(user)
-
-        const neededData = {
-            followerCount: user.followers.length,
-            followingCount: user.following.length,
-            bio: user.profile.bio,
-            website: user.profile.website,
-            avatar: user.profile.avatar
-        };
-
-        // res.json({user: neededData});
+    res.json({success: false})
+    AccountInfo.findOneAndUpdate(query, performUpdateAction, {'new': true})
+    .then((post) => {
+        AccountInfo.findOne(query, {'posts.$.liked': 1})
+        .then((data) => {
+            res.json({success: true});
+        });
     })
-    .catch( (err) => {
+    .catch((err) => {
+        res.json({success: false});
         console.log(err);
     });
 
 });
+
 
 module.exports = router;

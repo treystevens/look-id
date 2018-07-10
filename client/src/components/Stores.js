dfe45a6aaa4c1c025feb6cdcb0e7d4914e72bd7f
treykris <trey10027@gmail.com>
2018-07-10 11:48:21 -0400

    Slight clean up, store item bug still exists

--- client/src/components/Stores.js
+++ client/src/components/Stores.js
@@ -22,6 +22,7 @@ class Stores extends Component{
                 newStores[storeIndex] = evt.target.value;
                 
                 this.setState({ stores: newStores}, () => {
+                    console.log(this.state.stores)
                     this.props.handleStore(newStores, this.props.index);
                 })
             }
@@ -33,7 +34,10 @@ class Stores extends Component{
 
     // Remove Store from state
     handleRemoveStore = (index) => () => {
-        this.setState({ stores: this.state.stores.filter((store, storeIndex) => index !== storeIndex) });
+        this.setState({ stores: this.state.stores.filter((store, storeIndex) => index !== storeIndex) }, () => {
+            console.log(this.state.stores)
+            this.props.handleStore(this.state.stores, this.props.index);
+        });
     };
 
     // Adding store to our state
@@ -44,6 +48,11 @@ class Stores extends Component{
     }
 
     
+    // Current issues with this.
+    // If you add stores normally with deleting items it works fine.
+    // If you add stores and delete a store and dont type anything into the store, the database gets the store (the value will show the store of the one that was deleted, not the ones you need)
+    // If you add stores and delete one and then change the deleted one it will change the value inside the state
+    // Deleting an item also causes problem, it seems that the item is not really deleting and that the stores are still placed into the server. for instance i had three items, deleted the second, but the info from the second item was still sent to the server
 
 
     render(){
@@ -51,7 +60,7 @@ class Stores extends Component{
             <div>
                 {this.state.stores.map((store, index) => (
                     <div>
-                        <input type="text" placeholder={`Item #${index + 1} store location`} value={store.store}
+                        <input type="text" placeholder={`Item #${index + 1} store location`} value={this.state.store}
                             onChange={this.handleInputChange(index, `store`)} /> 
                         <button type="button" onClick={this.handleRemoveStore(index)}>x</button>
                     </div>

df11ab1e5a674cd9f49d6c49b547d010449e0aa4
treykris <trey10027@gmail.com>
2018-07-12 14:49:18 -0400

    Changed Schema to Users, added comments, removed setting profile default

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -5,24 +5,29 @@ const passport = require('passport');
 const { validationResult } = require('express-validator/check');
 const { body } = require('express-validator/check');
 
-// bycrypt
+// bcrypt
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 // Models
-const User = require('../models/user');
-const AccountInfo = require('../models/accInfo');
+const { Users } = require('../models/schemas');
 
 
+
+// When the app loads check if user is authenticated
 router.get('/', (req, res) => {
     
+    // If user is authenticated send back their data
     if(req.isAuthenticated()){
+
         const query = {
             username: req.user.username
         };
 
-        AccountInfo.findOne(query)
+        // Find authenticated user
+        Users.findOne(query)
         .then((data) => {
+
             const currentUser = {
                 username: req.user.username,
                 avatar: data.profile.avatar
@@ -34,32 +39,22 @@ router.get('/', (req, res) => {
             console.log(err);
         });
     }
+    // A guest is visiting the site
     else{
         res.json({isAuth: false});
     }    
-
-    
 });
 
-
-router.get('/login', (req, res) => {
-
-    res.json({actionSuccess: true});
-    
-    
-});
 
 router.post('/login', passport.authenticate('local'), (req, res, next) => {
 
     const query = { username: req.user.username };
-
     const currentUser = {
         username: req.user.username,
-        userID: req.user.id,
     };
 
     // Query to get the user's avatar
-    AccountInfo.findOne(query)
+    Users.findOne(query)
     .then((data) => {
 
         currentUser.avatar = data.profile.avatar;
@@ -72,10 +67,6 @@ router.post('/login', passport.authenticate('local'), (req, res, next) => {
 });
 
 
-router.get('/signup', (req, res) => {  
-    res.render('pages/register');
-});
-
 router.post('/signup', [
     body('username') // Username validation
     .isLength({ min: 3 })
@@ -84,13 +75,13 @@ router.post('/signup', [
     .isAlphanumeric()
     .withMessage('Username must have only letters or numbers.')
     .custom( (value, {req} ) => {
-        const usernameValue = value;
-        const usernameRegex = new RegExp(`^${usernameValue}$`);
-// at this point, the line above is the same as: var regex = /#abc#/g;
-        // Create an index for usernames for unique names
-        // return User.findOne({username: value})
-        return User.findOne({username: {$regex: usernameRegex, $options: 'i' }})
+    
+        const username = new RegExp(`^${value}$`);
+
+        return Users.findOne({username: {$regex: username, $options: 'i' }})
         .then((user) => {
+
+            // Checking to see if this username already exists
             if(user){
                 return Promise.reject(`The username "${req.body.username}" is already taken.`);
             }
@@ -101,7 +92,7 @@ router.post('/signup', [
         
     }),
     body('password') // Password validation
-    .isLength({ min: 3 })
+    .isLength({ min: 6 })
     .withMessage('Password must be at least 6 characters long.')
     .matches(/\d/)
     .withMessage('Password must have at least one number.'),
@@ -121,20 +112,16 @@ router.post('/signup', [
     
     // Errors are found on the form
     if(!validationErrors.isEmpty()){  
-        let mappedErrors = validationErrors.mapped();
-        console.log(mappedErrors, `mapped errors`);
 
-        console.log(req.body, `req body`);
-        
-        mappedErrors.usernameValue = req.body.username || '';
+        let mappedErrors = validationErrors.mapped();
 
         return res.status(422).json({ errors: mappedErrors });
-        // return res.json({ errors: mappedErrors });
-        // res.render('pages/register', {errMessage: mappedErrors});
     }
 
     // Place the new user into the database
     else{
+
+        // Information user filled out form with
         const user = {
             username: req.body.username,
             password: req.body.password
@@ -144,55 +131,26 @@ router.post('/signup', [
         bcrypt.hash(user.password, saltRounds).then( (hash) => {
             user.password = hash;
 
-            User.create(user).then((newUser) => {
-                const defaultAvatar = 'https://res.cloudinary.com/dr4eajzak/image/upload/v1530898955/avatar/default-avatar.jpg';
-
-                const defaultUserProfile = {
-                    profile: {
-                        avatar: defaultAvatar,
-                        bio: '',
-                        website: ''
+            // Place new user with hashed password into Users Collection
+            Users.create(user).then((newUser) => {
+ 
+                // Authenticate new user
+                req.login(newUser, (err) => {
+                    if (err) { 
+                        return next(err); 
                     }
-                };
-
-                let currentUser = {
-                    username: user.username,
-                    userID: user.id
-                };
-
-
-                // Save new user into Data Collection 
-                AccountInfo.create({username: user.username}).then(() => {
-                    AccountInfo.findOneAndUpdate({username: user.username},{$set: defaultUserProfile}, {new: true} ).then((data) => {
-                        console.log(data, `data to set the updated userprofile`);
-
-                        // Authenticate new user
-                        req.login(newUser, (err) => {
-                            if (err) { 
-                                return next(err); 
-                            }
-                            
-                            // Make sure avatar link is getting passed back so that we can have as a redux store state
-                            currentUser.avatar = data.profile.avatar;
-
-                            
-
-                            res.json({user: currentUser});
-                            
-                        });
-
-
-                        console.log(data);
-                    })
-                    .catch((err) => console.log(err));
-                });
-
-                
-                
-
-                
+                    
+                     // Make sure avatar link is getting passed back so that we can have as a redux store state
+                    const currentUser = {
+                        username: newUser.username,
+                        avatar: newUser.profile.avatar    
+                    };
+                   
+                    res.json({user: currentUser});
+                    
+                });                
             });
-        })
+        }) // end of bcrypt
         .catch((err) => {
             console.log(err);
         });

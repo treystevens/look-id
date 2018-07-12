b559002b42edfe913668818ce9b87e11ca975398
treykris <trey10027@gmail.com>
2018-07-12 12:16:37 -0400

    Following and follower feature

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -19,6 +19,18 @@ router.get('/:user', (req, res) => {
 
     //    console.log(user);
 
+    let alreadyFollow = false;
+
+       if(req.isAuthenticated()){
+            for(let me of user.followers){
+                console.log(me, `mweeee`)
+                if(req.user.username === me.username){
+                    alreadyFollow = true;
+                    console.log('were true')
+                }
+            }
+        }
+
        const neededData = {
            followerCount: user.followers.length,
            followingCount: user.following.length,
@@ -26,8 +38,15 @@ router.get('/:user', (req, res) => {
            website: user.profile.website,
            avatar: user.profile.avatar,
            posts: user.posts,
-           username: user.username
+           username: user.username,
+           alreadyFollowing: alreadyFollow
        };
+
+       console.log(`==========GET REQUEST FOLLOWERS=======`)
+       console.log(user.followers)
+       console.log(user.following)
+       
+       
 
        res.json({user: neededData});
    })
@@ -95,7 +114,9 @@ router.post('/:postid/like', (req, res) => {
         'posts.post_id': req.params.postid
     };
 
-    res.json({success: false})
+    // res.json({success: false})
+
+
     AccountInfo.findOneAndUpdate(query, performUpdateAction, {'new': true})
     .then((post) => {
         AccountInfo.findOne(query, {'posts.$.liked': 1})
@@ -110,5 +131,192 @@ router.post('/:postid/like', (req, res) => {
 
 });
 
+
+router.get('/:user/followers', (req, res) => {
+    
+    const query = {
+        username: req.params.username
+    };
+
+    AccountInfo.findOne(query, {followers: 1})
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+
+});
+
+router.post('/:user/followers', (req, res) => {
+    
+    const query = {
+        username: req.params.user
+    };
+
+    const myUserData = {
+        username: req.user.username,
+        avatar: req.body.myUserData.avatar
+    };
+
+    const addUser = {
+        username: req.params.user,
+        avatar: req.body.userToFollow.avatar
+    };
+
+    let myFollowingAction;
+
+    let updateAction;
+
+
+    if(req.body.myUserData.isFollowing){
+        updateAction = {$pull: {followers: myUserData} };
+        myFollowingAction = {$pull: {following: addUser} };
+        
+    }
+    else{
+
+        updateAction = {$push: {followers: myUserData} };
+        myFollowingAction = {$push: {following: addUser} };
+    }
+            
+
+
+    // Go to user profile add my data to their follower array
+    AccountInfo.findOneAndUpdate(query, updateAction, {new: true})
+    .then((data) => {
+
+        // Add user's data to my following array
+        AccountInfo.findOneAndUpdate({username: req.user.username}, myFollowingAction, {new: true})
+        .then((data) => {
+
+            let nowFollowing;
+            const prevFollowingData = req.body.myUserData.isFollowing;
+
+            // If we previously followed, we don't now
+            prevFollowingData ? nowFollowing = false : nowFollowing = true;
+
+
+            res.json({actionSuccess: true, isFollowing: nowFollowing});
+        });
+        
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+
+});
+
+
+router.get('/:user/ff/followers', (req, res) => {
+
+    console.log('got followers')
+    
+    const query = {
+        username: req.params.user
+    };
+
+    let projection = { followers: 1 };
+    let secondProjection = { following: 1};
+    
+
+
+    AccountInfo.findOne(query, projection)
+    .then((data) => {
+
+         
+
+        AccountInfo.findOne({username: req.user.username}, secondProjection)
+        .then((myFollowing) => {
+ 
+
+
+            // By sorting can probably speed this up
+            for(let i = 0; i < data.followers.length; i++){
+
+                for(let j = 0; j < myFollowing.following.length; j++){
+
+
+                    if(data.followers[i].username === myFollowing.following[j].username){
+                        data.followers[i].iFollow = true;
+                    }
+                }
+            }
+    
+            
+    
+            res.json({ff: data});
+        });
+
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+
+});
+
+router.get('/:user/ff/following', (req, res) => {
+    
+    const query = {
+        username: req.params.user
+    };
+
+    let projection = { following: 1};
+    
+
+    AccountInfo.findOne(query, projection)
+    .then((data) => {
+        let test = data;
+        
+        
+        for(let user of data.following){
+            
+            user.iFollow = true;
+        }
+
+        
+
+        res.json({ff: data});
+
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+
+});
+
+router.get('/:user/followers', (req, res) => {
+    
+    const query = {
+        username: req.params.user
+    };
+
+    let projection;
+    
+
+    if(req.params.follow === 'followers') projection = { followers: 1 };
+    if(req.params.follow === 'following') projection = { following: 1};
+    if(!req.params.follow) res.json({success: false});
+    
+
+    AccountInfo.findOne(query, projection)
+    .then((data) => {
+        
+
+    
+
+        res.json({ff: data});
+
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+
+});
 
 module.exports = router;

073a8a5d87472833442b8a10f2fd982c3dfd788d
treykris <trey10027@gmail.com>
2018-07-11 22:48:12 -0400

    Delete comment

--- client/src/components/Avatar.js
+++ client/src/components/Avatar.js
@@ -2,33 +2,14 @@ import React, { Component } from 'react';
 import { getData } from '../util/serverFetch';
 
 
-// class Avatar extends Component{
-//     constructor(props){
-//         super(props);
-
-//         this.state = {
-//             avatarUrl: ''
-//         };
-//     }
-
-
-//     render(){
-//         return(
-//             <div>
-//                 <img src={this.state.avatarUrl} alt=''
-//             </div>
-//         )
-//     }
-// }
-
-
 const Avatar = (props) => {
 
-    let imgAlt = `${props.userUsername}'s profile picture'`;
+    let imgAlt = `${props.username}'s profile picture'`;
+    
 
     return(
         <div style={{width: '80px', height: '80px', borderRadius: '50%', overflow:'hidden'}}>
-            <img src={props.avatarUrl} alt={imgAlt} style={{width: '100%'}}/>
+            <img src={props.avatar} alt={imgAlt} className='avatar' style={{width: '100%'}}/>
         </div>
     )
 }

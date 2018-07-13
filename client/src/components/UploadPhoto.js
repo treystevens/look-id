eaf493c89cd052f32ec8b94f10777efa52a37f8a
treykris <trey10027@gmail.com>
2018-07-13 14:58:29 -0400

    Created remove profile picture button and separated styling for avatar display and upload photo display

--- client/src/components/UploadPhoto.js
+++ client/src/components/UploadPhoto.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
+
  
 function handleFiles(evt) {
     let preview = document.querySelector('.preview');
@@ -10,30 +10,49 @@ function handleFiles(evt) {
     if(files && files[0]){
         let reader = new FileReader();
 
-        reader.onload = (e) => {
-            preview.setAttribute('src', e.target.result);
+        reader.onload = (evt) => {
+            preview.setAttribute('src', evt.target.result);
         };
 
         reader.readAsDataURL(files[0]);
     }
 
-  }
+}
+
+function removeAvatar(){
+    const file = document.querySelector('.photo-upload');
+    const defaultAvatar = 'https://res.cloudinary.com/dr4eajzak/image/upload/v1530898955/avatar/default-avatar.jpg';
+    let preview = document.querySelector('.preview');
+
+    file.value = '';
+    preview.setAttribute('src', defaultAvatar);
+}
 
 const UploadPhoto = (props) => {
 
     let containerClass;
     let inputName;
     let imgSrc;
-    
+    let showRemoveButton;
+       
 
     if(props.isAvatar){
-        containerClass = props.isAvatar;
+        let previewImgSrc = props.avatar;
+        
+        const defaultAvatar = 'https://res.cloudinary.com/dr4eajzak/image/upload/v1530898955/avatar/default-avatar.jpg';
+
+        if(previewImgSrc !== defaultAvatar){
+            showRemoveButton = true;
+        }
+
+        containerClass = 'avatar-container';
         inputName = 'user-avatar';
-        imgSrc = props.myAvatar;
+        imgSrc = props.avatar;
+        
 
     }
     if(props.isNewPost){
-        containerClass = props.isNewPost;
+        containerClass = 'upload-post-container';
         inputName = 'user-photo';
         imgSrc = '';
     }
@@ -41,19 +60,15 @@ const UploadPhoto = (props) => {
 
     return(
         <div>
-            <div className={containerClass} style = {{width: '120px',height: '120px', borderRadius: '50%', overflow: 'hidden'}}>
+            <div className={containerClass} >
                 <img src={imgSrc} className='preview' />
             </div>
-
-            <input type="file" className='fileUpload' name={inputName} onChange={handleFiles}/>
+            <input type="file" className='photo-upload' name={inputName} onChange={handleFiles}/>
+            {showRemoveButton && 
+            <button type='button' onClick={removeAvatar}>Remove Profile Picture</button>} 
         </div>
     )
 }
 
-function mapStateToProps(state){
-    return{
-        myAvatar: state.myAvatar
-    }
-}
 
-export default connect(mapStateToProps)(UploadPhoto);
+export default UploadPhoto;

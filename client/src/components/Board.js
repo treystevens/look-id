b196d26d5cb1db0a1239354f2d567b9837945650
treykris <trey10027@gmail.com>
2018-07-17 11:41:24 -0400

    Removed logs and unnecessary/unused variables

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -4,68 +4,41 @@ import PageHead from './PageHead';
 import { getData } from '../util/serverFetch';
 
 
-
 class Board extends Component{
     constructor(props){
         super(props);
 
         this.state = {
-            userProfileHead: {},
             streamData: [],
             boardName: ''
         };
     }
 
-
+    // Get Board data
     componentDidMount(){
 
-        console.log(this.props.urlParams);
-        const username = this.props.urlParams.match.params.user;
         const boardID = this.props.urlParams.match.params.boardid;
-        
-        console.log(boardID, username)
-
         const serverResponse = getData(`/board/${boardID}`);
 
         // Get Profile Data
         serverResponse.then(response => response.json())
         .then((data) => {
 
-
-            const userProfileHeadData = {
-                username: data.stream.username,
-            };
-
-            const streamUserData = {
-                posts: data.stream
-            };
-
-            console.log(data);
-            // For Outputting posts
-
             this.setState({
-                userProfileHead: userProfileHeadData,
-                streamData: streamUserData
-            }, () => {
-                console.log(this.state);
+                streamData: data.stream,
+                boardName: data.boardName
             });
-
         })
         .catch((err) => {
             console.log(err);
         });
-
-
     }
 
-
-      
     render(){
-        
         return(
             <section>
                 <PageHead pageHead={this.state.boardName} />
-                <Stream sourceFetch='explore' urlParams={this.state.userProfileHead.username} data={this.state.streamData}/>
+                <Stream sourceFetch='stream' stream={this.state.streamData} />
             </section>
         )
     }

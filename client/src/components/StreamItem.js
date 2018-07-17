116457bbb9b3d42dbe6d2ea772e8ba774966cff6
treykris <trey10027@gmail.com>
2018-07-17 12:10:33 -0400

    Added variables for readability, fixed alt for image

--- client/src/components/StreamItem.js
+++ client/src/components/StreamItem.js
@@ -1,32 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-/* jshint ignore:start */
-
-
 const StreamItem = (props) => {
 
-    console.log(props)
-    
-    let profileSrc = `/user/${props.username}`;
-    let postLink = `/user/${props.username}/${props.stream.post_id}`;
-    let imgSrc = `${props.stream.image}`;
+    const username = props.post.username;
+    const postID = props.post.post_id;
+    const postImage = props.post.image;
+    const profileSrc = `/user/${username}`;
+    const postLink = `/user/${username}/${postID}`;
+    const imgSrc = `${postImage}`;
 
     return(
         <div className="testMore">
             {props.usernameHeader && 
-            <h4><Link to={profileSrc}>{props.username}</Link></h4>
+            <h4><Link to={profileSrc}>{username}</Link></h4>
             }
             <Link to={postLink}>
-                <img src={imgSrc} alt="outfit"/>
+                <img src={imgSrc} alt={`${username}'s outfit.'`}/>
             </Link>
         </div>
     )
 }
 
-
 export default StreamItem;
 
 
-// Code here will be ignored by JSHint.
-/* jshint ignore:end */

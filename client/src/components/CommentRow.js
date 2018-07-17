f7f0ff31b9261f0758218193c9f4413f3cda1dab
treykris <trey10027@gmail.com>
2018-07-17 11:59:30 -0400

    Improved readability, avatar prop added

--- client/src/components/CommentRow.js
+++ client/src/components/CommentRow.js
@@ -4,44 +4,63 @@ import { Link } from 'react-router-dom';
 
 const CommentRow = (props) => {
 
+    const username = props.comment._user.username;
+
     let authorizedUser;
     let viewingUserAuth;
+    let avatar;
 
 
+    if(props.comment._user.profile.avatar){
+        avatar = props.comment._user.profile.avatar;
+    }
+
+    // User visiting a post where that they commented on
     if(props.viewingUser === props.comment.username){
         viewingUserAuth = true;
     }
 
-    // If the user is viewing their own profile avoid having two delete comment options
+    // If the user is viewing their own profile 
     if(props.urlParams.username === props.username && viewingUserAuth !== true){
         authorizedUser = true;
     }
 
-    
-    function deleteThis(evt){
-        
+    // Passing the index and keyID up a component
+    function deleteComment(evt){
         let keyID = evt.target.getAttribute('data-item-id');
-        props.handleDeleteComment(keyID);
+        props.handleDeleteComment(props.index, keyID);
     }
 
 
     return(
         <div className="comment-row" style={{width: '100%'}}>
-        <Link to={`/user/${props.comment.username}`}><img className="user-avatar" src={props.comment.avatar}/></Link>
+
+        <Link to={`/user/${props.comment.username}`}>
+            <img className="user-avatar" src={avatar}/>
+        </Link>
+
             <div>
                 <div className="user-data">
-                    <Link to={`/user/${props.comment.username}`}><span>{props.comment.username} </span></Link>
+                    <Link to={`/user/${username}`}>
+                        <span>{username} </span>
+                    </Link>
                     <span style={{color: "gray", fontSize: ".5rem"}}>{props.comment.date_posted}</span>
                 </div>
+
                 <div className="user-comment">
                     <p>{props.comment.comment}</p>
                 </div>
-                { authorizedUser &&
-                <div className="delete-comment" style={{color: "red"}} onClick={deleteThis} data-item-id={props.comment._id}>Delete Comment!</div>}
-                { viewingUserAuth &&
-                <div className="delete-comment" style={{color: "red"}} onClick={deleteThis} data-item-id={props.comment._id}>Delete Comment!</div>}
-            </div>
 
+                { authorizedUser &&
+                    <div className="delete-comment" style={{color: "red"}} onClick={deleteComment} data-item-id={props.comment._id}>Delete Comment!
+                    </div>
+                }
+
+                { viewingUserAuth &&
+                    <div className="delete-comment" style={{color: "red"}} onClick={deleteComment} data-item-id={props.comment._id}>Delete Comment!
+                    </div>
+                }
+            </div>
         </div>
     )
 }

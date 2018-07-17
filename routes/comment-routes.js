d685ecb337031338f07500bcc229fb29f3bd12fe
treykris <trey10027@gmail.com>
2018-07-17 12:34:01 -0400

    Updated comment routes to updated schema, populate users for avatar

--- routes/comment-routes.js
+++ routes/comment-routes.js
@@ -1,95 +1,79 @@
 const router = require('express').Router();
-const AccountInfo = require('../models/accInfo');
+const models = require('../models/schemas');
 
 
-
-
+// Initial load of user's Post, get 5 comments if available
 router.get('/:user/:postid', (req, res) => {
     
-    AccountInfo.findOne({'posts.post_id': req.params.postid}, {'posts.$.comments': 1, '_id': 0})
+    models.Posts.findOne({'post_id': req.params.postid}).populate({path: 'comments._user', model:'user', select:'profile.avatar username' }).exec()
     .then((data) => {
 
-
-        let commentsForPost = data.posts[0].comments.slice(0, 5);
-
-        console.log(commentsForPost)
+        const commentsForPost = data.comments;
         res.json({comments: commentsForPost});
     })
     .catch((err) => {
         console.log(err);
     });
-
-
 });
 
 
-router.get('/:user/:postid/viewall', (req, res) => {
-    
-    AccountInfo.findOne({'posts.post_id': req.params.postid}, {'posts.$.comments': 1, '_id': 0})
-    .then((data) => {
-
-        let commentsForPost = data.posts[0].comments;
-
-        res.json({comments: commentsForPost});
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
-
-});
-
-
+// User comments on post 
 router.post('/', (req, res) => {
 
-    // Do validation to make sure the a submit cannot be made if the comment box is empty
-
+    // If user submits empty comment
     if(req.body.newComment.comment === ''){
-        res.json({success: fail});
+        res.status(411).json({success: false});
     }
 
     const query = {
-        "posts.post_id": req.body.userPage.postID
+        "post_id": req.body.userPage.postID
     }; 
 
-
- 
-    AccountInfo.findOneAndUpdate(query, {$push: {"posts.$.comments": req.body.newComment}}, {
-        "new": true
-       })
-    .then( (data) => {
-        AccountInfo.findOne(
-            query, {"posts.$": 1})
-            .then((data) => {
-                console.log(data);
-
-                const databaseComments = data.posts[0].comments;
-                const length = data.posts[0].comments.length;
-
-                const newComment = databaseComments[length-1];
-
-                res.json({success: true, comment: newComment});
-
-                
-            });
-
-    
-
+    // Find my document in database and get ID
+    models.Users.findOne({username: req.user.username})
+    .then((user) => {
         
+        return user.id;
+    })
+    .then((userID) => {
+
+        // Store my ID into new comment for reference (populate)
+        const newComment = {
+            _user: userID,
+            date_posted: req.body.newComment.date_posted,
+            comment: req.body.newComment.comment
+        };
+
+        // Push new comment into post that was commented on
+        return models.Posts.findOneAndUpdate(query, {$push: {comments: newComment}});
         
     })
-    .catch( (err) => {
+    .then(() => {
+
+        // Query for the new comment on that post
+        return models.Posts.findOne(query).populate({path: 'comments._user', model:'user', select:'profile.avatar' }).exec();
+    })
+    .then((data) => {
+
+        // Get the comment from the query
+        const databaseComments = data.comments;
+        const length = data.comments.length;
+        const newComment = databaseComments[length-1];
+        
+        res.json({success: true, comment: newComment});
+    })
+    .catch((err) => {
         console.log(err);
     });
-
 });
 
 
+// User deletes a comment
 router.post('/delete', (req, res) => {
     
-    const query = { "posts.comments._id": req.body.id}; 
+    const query = { 'comments._id': req.body.id}; 
     
-    AccountInfo.update(query, { $pull: { 'posts.$.comments': {
+    models.Posts.update(query, { $pull: { 'comments': {
         "_id":req.body.id}}})
     .then( () => {
         res.json({success: true});
@@ -97,8 +81,7 @@ router.post('/delete', (req, res) => {
     .catch((err) => {
         console.log(err);
     });
-
-})
+});
 
 
 module.exports = router;

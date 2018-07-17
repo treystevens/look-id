d3549c03073eb1f37a246705bf3e89193c1259b3
treykris <trey10027@gmail.com>
2018-07-17 11:57:48 -0400

    added comments, changed variable declarations, properly reset comment box after submit

--- client/src/components/CommentBox.js
+++ client/src/components/CommentBox.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import Modal from './Modal';
 
 
-
 class CommentBox extends Component{
     constructor(props){
         super(props);
@@ -19,7 +18,7 @@ class CommentBox extends Component{
         this.closeModal = this.closeModal.bind(this);
     }
 
-    
+    // Close Modal (Login/Sign Up)
     closeModal(evt){
         if(evt.target.className === 'modal'){
             this.setState({
@@ -28,57 +27,68 @@ class CommentBox extends Component{
         }   
     }
 
-
+    // Submit new comment
     handleSubmit(evt){
         evt.preventDefault();
+        
+        // Empty comment box
+        const commentBox = document.querySelector('.comment__box');
+        commentBox.value = '';
+        this.setState({
+            commentValue: ''
+        });
 
-
+        // If user is not authorize prompt login Modal
         if(!this.props.isAuth){
             this.setState({
                 showModal: true
             });
+            return -1;
         }
 
+        // Error check if blank message is entered
         if(this.state.commentValue === ''){
             this.setState({
                 blankSubmit: true
             });
+            return -1;
         }
 
-        let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        
 
-        let currentDate = new Date();
+        // Set up date user commented
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        const currentDate = new Date();
+        const dayDate = currentDate.getDate();
+        const monthIndex = currentDate.getMonth();
+        const year = currentDate.getFullYear();
+        const month = months[monthIndex];
+        const date = `${month} ${dayDate}, ${year}`;
 
-        let dayDate = currentDate.getDate();
-        let monthIndex = currentDate.getMonth();
-        let year = currentDate.getFullYear();
-        let month = months[monthIndex];
-
-        let date = `${month} ${dayDate}, ${year}`;
-
+        // Data from page user is currently on
         const userPage = {
             username: this.props.urlParams.username,
             postID: this.props.urlParams.postID
         };
 
+        // User commenting data
         const newComment = {
-            username: this.props.username, // get from auth token I guess...from cookie?
+            username: this.props.username,
             date_posted: date,
             comment: this.state.commentValue,
-            avatar: this.props.myAvatar
         };
 
+        // Send this data to server
         const data = {
             newComment: newComment,
             userPage: userPage
         };
 
-
+        // Server Fetch
         const serverResponse = sendUserData('/comment', data);
 
         serverResponse.then(response => response.json())
         .then((data) => {
-            console.log(data, `response from comment`);
 
             // Response from server checking if it's an empty comment
             if(!data.success){
@@ -86,15 +96,18 @@ class CommentBox extends Component{
                     blankSubmit: true
                 });
             }
-
-            // pass back the data needed to display the comment
-            this.props.handleAddComment(data.comment);
+            else{
+                // Pass the newly created comment up to Comments Component
+                this.props.handleAddComment(data.comment);
+            }
+            
         })
         .catch((err) => {
             console.log(err);
         });
     }
 
+    // Comment input value
     handleComment(evt){
         this.setState({
             commentValue: evt.target.value
@@ -102,13 +115,15 @@ class CommentBox extends Component{
     }
 
     render(){
-        let blankSubmitMessage = 'Please enter a message';
+        const blankSubmitMessage = 'Please enter a message';
 
         return(
                 <div style={{width: '100%'}}>
                     <form onSubmit={this.handleSubmit} style={{display: 'flex', 'flexDirection': 'column'}}>
+
                         {this.state.blankSubmit &&
                         <span>{blankSubmitMessage}</span>}
+
                         <textarea className="comment__box" cols="50" rows="3" onChange={this.handleComment} name='comment'></textarea>
                         <button>Submit Comment</button>
                     </form>
@@ -120,13 +135,10 @@ class CommentBox extends Component{
 }
 
 
-
-// Might add an avatar to redux store for comments
 function mapStateToProps(state) {
     return {
       username: state.username,
       isAuth: state.isAuth,
-      myAvatar: state.avatar
     };
 }
 

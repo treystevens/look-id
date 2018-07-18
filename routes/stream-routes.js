4c7f6e525450c736ed3e31d6ce3c8cfbc75414d0
treykris <trey10027@gmail.com>
2018-07-18 00:09:30 -0400

    explore and feed routes added

--- routes/stream-routes.js
+++ routes/stream-routes.js
@@ -1,21 +1,59 @@
 const router = require('express').Router();
 const TestExplore = require('../models/testexplore');
+const models = require('../models/schemas');
 
 
-
+// Route for global explore feed
 router.get('/explore', (req, res) => {
     
-    TestExplore.find({})
-    .then((response) => {
-        console.log(response);
-        console.log(response[0].stream);
+    // Get documents back in the descending order
+    models.Explore.find({}).populate('post').sort({_id:-1}).exec()
+    .then((data) => {
 
-        res.json({stream: response[0].stream});
+        // Sending back only necessary post information
+        const posts = data.map((post) => {
+            const userPost = {
+                _id: post.id,
+                post_id: post.post.post_id,
+                username: post.post.username,
+                image: post.post.image
+            };
+            return userPost;
+        });
+        res.json({stream: posts});
     })
     .catch((err) => {
         console.log(err);
     });
+});
 
+
+// Route for a user's feed
+router.get('/feed', (req, res) => {
+
+    const query = {
+        username: req.user.username
+    };
+    
+    // Get user's feed items
+    models.Feed.findOne(query).populate('feed_items').exec()
+    .then((data) => {        
+
+        // Sending back only necessary post information
+        const posts = data.feed_items.map((post) => {
+            const userPost = {
+                _id: post.id,
+                post_id: post.post_id,
+                username: post.username,
+                image: post.image
+            };
+            return userPost;
+        });
+        res.json({stream: posts});
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 });
 
 module.exports = router;

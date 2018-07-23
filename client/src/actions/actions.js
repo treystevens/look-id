3d7aa8439d9ca7ee54ffd4dd7d24b7e2ac0004a7
treykris <trey10027@gmail.com>
2018-07-23 12:47:58 -0400

    Removed update avatar action

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -3,15 +3,8 @@ function addAuth(user){
     return {
         type: 'AUTHENTICATE', 
         user: user.username,
-    };
-   
+    }; 
 }
 
-function updateAvatar(avatarLink){
-    return {
-        type: 'UPDATE_AVATAR', 
-        updateAvatar: avatarLink
-    };
-}
 
-export { addAuth, updateAvatar };
+export { addAuth };

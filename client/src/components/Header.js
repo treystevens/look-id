ef3b4f6cf156bcb385e4b99746ca4c6bb21ce226
treykris <trey10027@gmail.com>
2018-07-22 20:43:32 -0400

    Removed comments

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import AccountHead from './AccountHead';
-import { NavLink } from 'react-router-dom';
-// import '../../public/sheets/styles.css';
+import { NavLink, Link } from 'react-router-dom';
+
+
 
 /* jshint ignore:start */
 
@@ -14,35 +15,16 @@ class Header extends Component{
     }
     // Have a state of wether desktop and mobile to pass to Hamburger menu and sign in/ sign up
 
-    componentDidMount(){
-        'we mounted'
-
-        // fetch('/auth',{
-        //     method: 'GET'
-        // })
-        // .then((res) => {
-        //     console.log(res)
-        //     return res.json()
-        // })
-        // .then((user) => {
-        //     console.log(user)
-        // })
-        // .catch((err) => {
-        //     console.log(err)
-        // })
-    }
 
     render(){
         return(
             <header>
                 <div>
-                        <h1>LookID.</h1>
+                        <Link to='/'><h1>LookID.</h1></Link>
                     <nav>
                         <ul>
-                            {/* <li><a href="explore">Explore</a></li>
-                            <li><a href="feed">Feed</a></li>
-                            <li><a href="boards">Boards</a></li> */}
-                            <li><NavLink to='/'>Explore</NavLink></li>
+                            <li><NavLink to='/'>Home</NavLink></li>
+                            <li><NavLink to='/explore'>Explore</NavLink></li>
                             <li><NavLink to='/feed'>Feed</NavLink></li>
                             <li><NavLink to='/boards'>Boards</NavLink></li>
                         </ul>

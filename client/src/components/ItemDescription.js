1cf5e69308a53ef76186df05f146a5ddd4fb64bb
treykris <trey10027@gmail.com>
2018-07-23 12:49:15 -0400

    prepend https to link if not present in link

--- client/src/components/ItemDescription.js
+++ client/src/components/ItemDescription.js
@@ -1,27 +1,45 @@
 import React from 'react';
-// import FindStores from './FindStores'
-
 
 const ItemDescription = (props) => {
-    console.log(props);
-    console.log(props.item.stores);
+
+    const prefix = 'https://';
+    let itemLink = props.item.link;
+
+
+    // prepend https to link if not present
+    if(itemLink){
+        if (!props.item.link.match(/^[a-zA-Z]+:\/\//))
+        {
+        itemLink = `${prefix}${props.item.link}`;
+        }
+    }
+
+    // Map out the stores ( they are passed as an array of stores )
+    const stores = props.item.stores.map((store, index) => {
+        
+        return <div key={`${props.itemNumber}-store${index}`}>{store}</div>
+    }); 
+
 
     return(
         <article style={{display: 'flex', flexDirection: 'column'}}>
             <h3>{props.item.name}</h3>
-            <span>${props.item.price}</span>
+
+            {props.item.price &&
+                <span>${props.item.price}</span>
+            }
+
             {props.item.thrifted && 
                 <span>Thrifted</span>}
+
             <div style={{display: 'flex'}}>
-                {props.item.link && 
-                    <a href={`http://www.${props.item.link}`} target='_blank'>Link to Item</a>
+
+                {itemLink && 
+                    <a href={itemLink} target='_blank'>Link to Item</a>
                 }
-                <div style={{display: 'flex', flexDirection: 'column'}}>
-                {props.item.stores.map((store) => {
-                    return <span>{store}</span>
-                }) }
-                </div>
-                {/* <FindStores stores={props.item.stores} /> */}
+                
+                {stores}
+       
             </div>
         </article>
     )

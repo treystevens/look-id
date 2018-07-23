3c7dfc62a41ed7752ba4bb50c2dd0e3a0e66efc7
treykris <trey10027@gmail.com>
2018-07-23 17:48:59 -0400

    Fixed bug where editing a board shows the first board a user created / board display image changes if the user deletes the same image from board

--- routes/board-routes.js
+++ routes/board-routes.js
@@ -5,6 +5,7 @@ const models = require('../models/schemas');
 // Get Boards from user
 router.get('/', (req, res) => {
 
+    console.log(req.user);
     const query = {
         username: req.user.username
     };
@@ -69,16 +70,35 @@ router.post('/addpost', (req, res) => {
 
 
     // Query the post to get the _id to save into Board's 'posts' property
-    models.Posts.findOne(postToAdd)
+    models.Posts.findOne(postToAdd) 
     .then((post) => {
-
+        console.log(post, `actual post`)
         const referenceToPost = post._id;
-        return referenceToPost;
+        // Check if the post exists in the board already
+        // const checkPost = models.Users.findOne({'boards.posts': referenceToPost});
+        const checkPost = models.Users.findOne(
+            { boards: { $elemMatch: { board_id: req.body.boardID, posts: referenceToPost } }});
+
+        // { results: { $elemMatch: { product: "xyz", score: { $gte: 8 } } } }
+        // return referenceToPost;
+        // console.log(referenceToPost, `reference to post`)
+
+        return Promise.all([referenceToPost, checkPost]);
     })
-    .then((postID) => {
+    .then((results) => {
 
-       return models.Users.findOneAndUpdate(query, {$push: {'boards.$.posts': postID}});
 
+        
+        // Does not exist inside board
+        // Reference post was not already found inside the board - clear to add post to board
+        if(results[1] === null){
+            return models.Users.findOneAndUpdate(query, {$push: {'boards.$.posts': results[0]}});   
+        }
+
+        // The particular image is already inside the board the user would like to add to
+        return Promise.reject('Post already exists inside board');
+       
+    
     })
     .then(() => {
 
@@ -90,11 +110,11 @@ router.post('/addpost', (req, res) => {
     })
     .catch((err) => {
         console.log(err);
-        res.status(500).json({errors: 'Could not add post to the board at the moment'});
+        res.json({errors: err});
     });
 });
 
-// Get individual board
+// Get individual board (Infinite Scroll)
 router.get('/:boardid/page/:page', (req, res) => {
     
     const query = {
@@ -123,37 +143,104 @@ router.get('/:boardid/page/:page', (req, res) => {
     });
 });
 
-// Get individual board
-router.post('/:boardid/edit', (req, res) => {
-
-    console.log(req.body.posts);
+// Get individual board (Infinite Scroll)
+router.get('/:boardid', (req, res) => {
     
     const query = {
         'boards.board_id': req.params.boardid
     };
 
-    if(!req.isAuthenticated()) res.status(402);
+
+    models.Users.findOne(query, {'boards.$.posts': 1}).populate({ path: 'boards.posts', populate: {path: 'posts', model: 'post'}, select: 'username image post_id' }).exec()
+    .then((response) => {
+
+        const boardName = response.boards[0].name;
+        const posts = response.boards[0].posts;
+
+
+        res.json({stream: posts, boardName: boardName});
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json({errors: 'Failed to retrieve board.'});
+    });
+});
+
+// Get individual board
+router.post('/:boardid/edit', (req, res) => {
 
     
+    const query = {
+        'boards.board_id': req.params.boardid
+    };
+    const listOfPostIDs = req.body.posts.map((post) => post.postID);
+    const listOfPostImages = req.body.posts.map((post) => post.postImg);
 
+
+    // If user is not authenticated
+    if(!req.isAuthenticated()) res.status(402);
+
+    // Update database on deleted posts or board name change
     const pullPosts = models.Users.findOneAndUpdate(
         query,  
-        { $pull: { 'boards.$.posts': { $in: req.body.posts }   }  },
-        {new: true}).populate({ path: 'boards.posts', populate: {path: 'posts', model: 'post'}, select: 'username image post_id' }).exec()
+        { $pull: { 'boards.$.posts': { $in: listOfPostIDs }   }  })
+        .populate({ path: 'boards.posts', populate: {path: 'posts', model: 'post'}, select: 'username image post_id' }).exec();
 
     const changeName = models.Users.findOneAndUpdate(
         query,
-        { 'boards.$.name': req.body.boardName}, {new: true}
+        { 'boards.$.name': req.body.boardName}
     );
 
     Promise.all([pullPosts, changeName])
-    .then((response) => {
+    .then(() => {
 
-        console.log(response)
-        const boardName = response[1].boards[0].name;
-        const posts = response[0].boards[0].posts;
+        // Query for document again and use $ to project the changed board
+        return models.Users.find(query, {'boards.$': 1} ).populate({ path: 'boards.posts', populate: {path: 'posts', model: 'post'}, select: 'username image post_id' }).exec();
+    })
+    .then((result) => {
 
-        console.log(boardName)
+        const board = result[0].boards[0];
+        const boardName = board.name;
+        const posts = board.posts;
+        let changeDisplay;
+
+
+        // Check if a image that was requested to be deleted is the board's display image
+        listOfPostImages.forEach((image) => {
+            if(image === board.display_image) changeDisplay = true;
+        });
+
+        // One of the that was deleted is the same as the board's display image
+        if(changeDisplay){
+
+            // Empty string or a new post image for board display image
+            let updateAction;
+
+            if(posts.length === 0) updateAction = '';
+            if(posts.length > 0) updateAction = posts[0].image;
+
+            return models.Users.findOneAndUpdate(
+                query,
+                { 'boards.$.display_image': updateAction}
+            );
+        }
+
+        // Post image was not the same as board display
+        else{
+            res.json({stream: posts, boardName: boardName});
+        }
+    })
+    .then(() => {
+
+        // Query for the specific board again
+        return models.Users.find(query, {'boards.$': 1} ).populate({ path: 'boards.posts', populate: {path: 'posts', model: 'post'}, select: 'username image post_id' }).exec();
+    })
+    .then((result) => {
+        
+        const board = result[0].boards[0];
+        const boardName = board.name;
+        const posts = board.posts;
+
         res.json({stream: posts, boardName: boardName});
     })
     .catch((err) => {

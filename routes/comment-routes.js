54133a83a6a3959f99c402c6fb33b2f668324194
treykris <trey10027@gmail.com>
2018-07-25 13:03:45 -0400

    Error handling and proper authentication for checking if user can delete a comment

--- routes/comment-routes.js
+++ routes/comment-routes.js
@@ -8,14 +8,16 @@ router.get('/:user/:postid', (req, res) => {
     models.Posts.findOne({'post_id': req.params.postid}).populate({path: 'comments._user', model:'user', select:'profile.avatar username' }).exec()
     .then((data) => {
 
+        if(!data) return Promise.reject(new Error('Had a problem with loading comments for this post.'));
+
         const commentsForPost = data.comments;
         res.json({comments: commentsForPost});
     })
     .catch((err) => {
+        res.status(500).json({error: err});
         console.log(err);
     });
 });
-
 
 // User comments on post 
 router.post('/', (req, res) => {
@@ -51,10 +53,10 @@ router.post('/', (req, res) => {
     .then(() => {
 
         // Query for the new comment on that post
-        return models.Posts.findOne(query).populate({path: 'comments._user', model:'user', select:'profile.avatar' }).exec();
+        return models.Posts.findOne(query).populate({path: 'comments._user', model:'user', select:'profile.avatar username' }).exec();
     })
     .then((data) => {
-
+        
         // Get the comment from the query
         const databaseComments = data.comments;
         const length = data.comments.length;
@@ -63,24 +65,48 @@ router.post('/', (req, res) => {
         res.json({success: true, comment: newComment});
     })
     .catch((err) => {
+        res.json({error: err});
         console.log(err);
     });
 });
 
-
 // User deletes a comment
 router.post('/delete', (req, res) => {
-    
+
+
     const query = { 'comments._id': req.body.id}; 
-    
-    models.Posts.update(query, { $pull: { 'comments': {
-        "_id":req.body.id}}})
+
+    // See if the user wanted to delete is authorized to delete comment
+    models.Posts.findOne(query, { 'comments.$._id': 1, username: 1 })
+    .then((post) => {
+
+        let masterUser;
+        let visitingUser;
+        
+        if(req.user.username === post.username) masterUser = true;
+        if(req.user.id == post.comments[0]._user) visitingUser = true;
+
+        // Pull comment out of post
+        if(masterUser || visitingUser){
+            return models.Posts.update(query, { $pull: { 'comments': { "_id":req.body.id }}});
+        }
+        else{
+            return Promise.reject(new Error('You\'re not authorized to delete that comment.'));
+        }
+        
+    })
     .then( () => {
-        res.json({success: true});
+        res.status(200).json({success: true});
     })
     .catch((err) => {
+        res.status(401).json({error: err.message});
         console.log(err);
     });
+
+
+    // have a username is isauth sent
+    // if the username = req.user.username we good to delete comment
+    // if is isatuh its good to delete any comment as long as the page they were on is rqual to req.user.usernam
 });
 
 

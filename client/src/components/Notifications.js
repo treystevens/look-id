0cfc0451b6fc06f9d087ec620aaba30e72ac7d66
treykris <trey10027@gmail.com>
2018-07-27 13:15:36 -0400

    handle notification data

--- client/src/components/Notifications.js
+++ client/src/components/Notifications.js
@@ -1,47 +1,129 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom'; 
+import { getData } from '../util/serverFetch';
+import ConfirmAction from './ConfirmAction';
+import PageHead from './PageHead';
+import NotificationItem from './NotificationItem';
 
 
 class Notifications extends Component{
     constructor(props){
-        super(props)
+        super(props);
+
+        this.state = {
+            notifications: [],
+            showConfirmation: '',
+            actionSuccess: false,
+            statusMessage: '',
+            isLoading: false,
+            hasMore: true,
+            scrollCount: 0
+        };
+
+        this.onScroll = this.onScroll.bind(this);
+        this.loadData = this.loadData.bind(this);
     }
 
-    // componentDidUpdate(){
-    //     console.log(`notifications did update`)
-    // }
-
-    // or
     componentDidMount(){
-        // get the notification data for this year
-        console.log(`notifications did mount, AS IT SHOULD IT"S INSIDE THE ACCOUNT HEADER`)
+        window.addEventListener('scroll', this.onScroll);
+
+
+        // Set viewed property on database to true
+        fetch('/user/notifications', {
+            method: 'PUT',
+            credentials: 'include'
+        })
+        .then(response => response.json())
+        .then(() => {
+            
+            this.loadData();
+        })
+        .catch((err) => {
+            this.setState({
+                showConfirmation: true,
+                statusMessage: err.message
+            });
+            console.log(err);
+        });
+    }
+   
+    // Remove event listener
+    componentWillUnmount(){
+        window.removeEventListener('scroll', this.onScroll);
     }
 
-    // INside comments have it fetch to the database to save the comment, save the comment ind atabase get back the data that it's successfully saved and then pass in the response to a prop that we get here from the comment component.. Save for the Like component
-    // componentWillReceiveProps(){
-    //     console.log(`notifications will recieve props`)
-    //     console.log(this.props.notifications)
-    // }
+    loadData(){
+        this.setState({
+            isLoading: true
+        });
 
-    // shouldComponentUpdate(){
+        const { scrollCount, notifications } = this.state;
         
-    //         console.log(`should component update`)
-    //         console.log(this.props.notifications)
+        const serverResponse = getData(`/user/notifications/v/${scrollCount}`);
+
+        serverResponse
+        .then(response => response.json())
+        .then((data) => {
+            if(data.error) return Promise.reject(new Error(data.error));
+
+            this.setState({
+                notifications: notifications.concat(data.notifications),
+                hasMore: data.hasMore,
+                isLoading: false
+            });
+        })
+        .catch((err) => {
+            this.setState({
+                showConfirmation: true,
+                statusMessage: err.message
+            });
+            console.log(err);
+        });
+
+
+    }
+
+    // For Infinite Scroll
+    onScroll(){
         
-    // }
+        const  { error, isLoading, hasMore } = this.state;
+        
+
+        // Return if there's an error, already loading or there's no more data from the database
+        if (error || isLoading || !hasMore) return;
+
+        // Check if user has scrolled to the bottom of the page
+        if (window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight - 150) {
+            
+            // Using scrollCount to mimic pages
+            const { scrollCount } = this.state;
+            const newCount = scrollCount + 1;
+            
+            
+            this.setState({
+                scrollCount: newCount
+            }, () => {
+                this.loadData();
+            });  
+        }
+    }
 
     render(){
-        return(
-            <li>
-                        <NavLink to='/profile/notifications'>
-                            <div style={{width: '20px', height: '20px', border: '1px solid gray', borderRadius: '2px', position: 'relative'}}>
 
-                                <div style={{position: 'absolute', right: '-5px', top: '-5px', backgroundColor: 'green', borderRadius: '50%', width: '10px', height: '10px'}}>
-                                
-                                </div>
-                            </div>
-                        </NavLink>
-                    </li>
+        const { notifications } = this.state;
+
+        const showNotifications = notifications.map((notification) => {
+            return <NotificationItem notification={notification}  key={notification._id}/>
+        });
+
+        return(
+            <section style={{display: 'flex', flexDirection:'column'}}>
+                <PageHead pageHead='Notifications' />
+
+                {this.state.showConfirmation &&
+                        <ConfirmAction actionSuccess={this.state.confirmAction} statusMessage={this.state.statusMessage}/>
+                    }
+                { showNotifications }
+            </section>
         )
     }
 }

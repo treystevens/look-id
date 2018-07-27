363999dee68e1019670f8c1ae19a872a963474f9
treykris <trey10027@gmail.com>
2018-07-27 13:15:52 -0400

    removed unused variable

--- client/src/components/OtherPosts.js
+++ client/src/components/OtherPosts.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
 
-
-
 const OtherPosts = (props) => {
-    // just make a map of the last three posts
-    let profileSrc = `/user/${props.username}`;
-    let imgSrcLink = `/user/${props.username}/${props.post.post_id}`;
     
-
+    const imgSrcLink = `/user/${props.username}/${props.post.post_id}`;
+    
     return(
         <a href={imgSrcLink}>
             <img src={props.post.image} alt='Other posts for this user' style={{'width': '100%'}}/>

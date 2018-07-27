44c72d0237622c48912cf920148461052028a23f
treykris <trey10027@gmail.com>
2018-07-27 13:18:44 -0400

    append a prefix to URLs

---
+++ client/src/util/general.js
@@ -0,0 +1,18 @@
+function prefixURL(url){
+    console.log(url);
+
+    if(!url) return url;
+    
+    const prefix = 'https://';
+    let newURL = url;
+
+    if (!url.match(/^[a-zA-Z]+:\/\//))
+    {
+    newURL = `${prefix}${url}`;
+    }
+
+    return newURL;
+}
+
+export { prefixURL };
+

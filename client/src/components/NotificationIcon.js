fba4e714f6444f8231dea89db6b2ce0a8bebd286
treykris <trey10027@gmail.com>
2018-07-27 13:14:59 -0400

    Notification icon for seeing if new notifications are present

---
+++ client/src/components/NotificationIcon.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom'; 
+import { getData } from '../util/serverFetch';
+import { connect } from 'react-redux';
+
+
+class NotificationIcon extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            newNotifications: false
+        };
+
+        this.loadNotifications = this.loadNotifications.bind(this);
+    }
+
+
+    // Check for new notifications
+    componentDidMount(){
+        this.loadNotifications();
+    }
+
+    
+    componentDidUpdate(prevProps){
+        if(this.props.notifications !== prevProps.notifications){
+            this.loadNotifications();
+        }
+    }
+
+    loadNotifications(){
+        const serverResponse = getData('/user/notifications-check');
+
+        serverResponse
+        .then(response => response.json())
+        .then((data) => {
+
+            this.setState({
+                newNotifications: data.newNotifications
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
+    render(){
+
+        const { newNotifications } = this.state;
+        let LED;
+
+        newNotifications ? LED = 'notifications__light' : LED = 'notifications__dull';
+
+        return(
+                <li>
+                    <NavLink to='/profile/notifications'>
+                        <div style={{width: '20px', height: '20px', border: '1px solid gray', borderRadius: '2px', position: 'relative'}}>
+
+                            <div className={LED} style={{position: 'absolute', right: '-5px', top: '-5px', borderRadius: '50%', width: '10px', height: '10px'}}> 
+                            </div>
+
+                        </div>
+                    </NavLink>
+                </li>
+        )
+    }
+}
+
+function mapStateToProps(state){
+    return{
+        username: state.username,
+        notifications: state.notifications
+    }
+}
+
+
+export default connect(mapStateToProps)(NotificationIcon);

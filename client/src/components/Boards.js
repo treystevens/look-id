b238f619be8c91e8b052e3b601a2fd6f423b2ae4
treykris <trey10027@gmail.com>
2018-08-01 20:36:51 -0400

    removed edit options, added confirmaction messages

--- client/src/components/Boards.js
+++ client/src/components/Boards.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import BoardItem from './BoardItem';
-import CreateBoard from './CreateBoard';
 import BoardModal from './BoardModal';
 import PageHead from './PageHead';
 import { getData, sendUserData, goDelete } from '../util/serverFetch';
 import { connect } from 'react-redux';
 import ConfirmAction from './ConfirmAction';
 import Modal from './Modal';
-
+import CreateElem from './CreateElem';
+import './Boards.css';
 
 class Boards extends Component{
     constructor(props){
@@ -21,34 +21,25 @@ class Boards extends Component{
             showConfirmation: false,
             showAccountVerify: false,
             showCreateBoard: false,
-            showOptions: false
+            showOptions: false,
+            boardID: ''
         };
 
         this.handleClickCreateBoard = this.handleClickCreateBoard.bind(this);
         this.closeModal = this.closeModal.bind(this);
         this.handleNewBoardSubmit = this.handleNewBoardSubmit.bind(this);
         this.handleClickBoard = this.handleClickBoard.bind(this);
-        this.handleDeleteBoard = this.handleDeleteBoard.bind(this);
         this.showErrorStatus = this.showErrorStatus.bind(this);
-        this.showOptions = this.showOptions.bind(this);
-        
-
-        // Add key listener on window to close modal with 'esc' key
-        window.addEventListener('keydown', (evt) => {
-            if(this.state.showModal && evt.keyCode === 27){
-                this.setState({
-                    showModal: false,
-                    showAccountVerify: false,
-                    showCreateBoard: false
-                });
-            }
-        });
+        this.escModal = this.escModal.bind(this);
     }
 
     // Get all of user's boards
     componentDidMount(){
         
         const serverResponse = getData('/board');
+
+        // Add key listener on window to close modal with 'esc' key
+        window.addEventListener('keydown', this.escModal);
 
         serverResponse.then(response => response.json())
         .then((data) => {
@@ -67,20 +58,24 @@ class Boards extends Component{
 
     // Remove event listener
     componentWillUnmount(){
-        // Remove window listener
-        window.removeEventListener('keydown', (evt) => {
-            if(this.state.showModal && evt.keyCode === 27){
-                this.setState({
-                    showModal: false
-                });
-            }
-        });
+        window.removeEventListener('keydown', this.escModal);
+    }
+
+    // Close modal with esc key
+    escModal(evt){
+        if(this.state.showModal && evt.keyCode === 27){
+            this.setState({
+                showModal: false,
+                showAccountVerify: false,
+                showCreateBoard: false
+            });
+        }
     }
     
     // Close modal on click
     closeModal(evt){
-
-        if(evt.target.className === 'modal' || evt.target.className === 'modal__close-btn'){
+        
+        if(evt.target.className === 'modal' || evt.target.classList.contains('btn__close--modal') || evt.target.classList.contains('btn__cancel--modal')){
             this.setState({
                 showModal: false,
                 showAccountVerify: false,
@@ -132,7 +127,8 @@ class Boards extends Component{
             this.setState({
                 boards: this.state.boards.concat(data.boards),
                 showConfirmation: true,
-                statusMessage: 'Created new board!'
+                statusMessage: 'Created new board!',
+                actionSuccess: true
 
             });
         })
@@ -145,57 +141,48 @@ class Boards extends Component{
 
     // Show ConfirmAction if error occurred 
     showErrorStatus(err){
+        
         this.setState({
             showConfirmation: true,
-            statusMessage: err.errors
+            statusMessage: err.message
         });
     }
 
-    // Show Edit options
-    showOptions(){
-
-        const { showOptions } = this.state;
-
-        if(showOptions){
-            this.setState({
-                showOptions: false
-            });
-        }
-        else{
-            this.setState({
-                showOptions: true
-            });
-        }
-    }
 
     // For adding an image to the board from a post
     handleClickBoard(evt){
         
-        if(evt.target.classList.contains('boardImg')){
+        if(evt.target.classList.contains('board__display')){
             const boardID = evt.target.getAttribute('data-board-id');
             const postImageElement = document.querySelector('.post__image');
             const postImageSrc = postImageElement.getAttribute('src');
+            const boardName = evt.target.parentElement.childNodes[1].textContent;
 
             const data = {
                 username: this.props.urlParams.username,
                 postID: this.props.urlParams.postID,
                 boardID: boardID,
-                postImage: postImageSrc
+                postImage: postImageSrc,
+                boardName: boardName
             };
+
+            
+            
 
             const serverResponse  = sendUserData('/board/addpost', data);
             
             serverResponse.then(response => response.json())
             .then((data) => {
-
-                if(data.errors) return Promise.reject(new Error(data));
+                
+                if(data.error) return Promise.reject(new Error(data.error));
+                
 
                 // Make confirmation that it was added
                 if(data.success){
                     this.setState({
                         showConfirmation: true,
                         actionSuccess: true,
-                        statusMessage: 'Saved to board!'
+                        statusMessage: `Saved to "${boardName}"!`
                     });
                 }
             })
@@ -205,59 +192,28 @@ class Boards extends Component{
         }
     }
 
-    // Delete board - Click event
-    handleDeleteBoard(board){
-
-        const {board: {_id: boardID}} = board;
-
-        const serverResponse = goDelete(`/board/${boardID}/delete`);
-
-        serverResponse
-        .then(response => response.json())
-        .then((data) => {
-            
-            if(data.error) return Promise.reject(new Error(data.error));
-
-            this.setState({
-                boards: data.boards
-            });
-        })
-        .catch((err) => {
-            this.showErrorStatus(err);
-        });
-    }
-
 
 
     render(){
-        const { showAccountVerify, showConfirmation, showCreateBoard, showOptions } = this.state;
+        const { showAccountVerify, showConfirmation, showCreateBoard, actionSuccess } = this.state;
+
 
         const userBoards = this.state.boards.map((board) => {
             return (
-            <article key={board.board_id}>
-                <span onClick={this.showOptions}>...</span>
-                <div>
-                    {showOptions &&
-                    <ul>
-                        <li>Edit Board</li>
-                        <li onClick={this.handleDeleteBoard.bind(this, {board})}>Delete Board</li>
-                        
-                    </ul>
-                    }
-                </div>
-                <BoardItem key={board.board_id} board={board} addToBoard={this.props.addToBoard}  urlParams={this.props.urlParams} handleClickBoard={this.handleClickBoard}/>
-            </article>
+                <article key={board.board_id} className='board'>
+                    <BoardItem key={board.board_id} board={board} addToBoard={this.props.addToBoard}  urlParams={this.props.urlParams} handleClickBoard={this.handleClickBoard}/>
+                </article>
             ) })
 
         return(
-            <section>
+            <section className='container'>
                 <PageHead pageHead={this.props.pageName} />
 
                 {showConfirmation &&
-                <ConfirmAction statusMessage={this.state.statusMessage}/>}
+                <ConfirmAction statusMessage={this.state.statusMessage} actionSuccess={actionSuccess} absolute={true}/>}
 
-                <div className="boardContainer">
-                    <CreateBoard handleClickCreateBoard={this.handleClickCreateBoard} />
+                <div className="boards">
+                    <CreateElem handleCreate={this.handleClickCreateBoard} text='Create New Board'/>
                     {userBoards}
 
 

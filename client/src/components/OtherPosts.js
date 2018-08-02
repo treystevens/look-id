f3779496cf9ff16dad02133c43a68ea4d81074a7
treykris <trey10027@gmail.com>
2018-08-01 20:54:40 -0400

    added classes

--- client/src/components/OtherPosts.js
+++ client/src/components/OtherPosts.js
@@ -6,9 +6,11 @@ const OtherPosts = (props) => {
     const imgSrcLink = `/user/${props.username}/${props.post.post_id}`;
     
     return(
-        <a href={imgSrcLink}>
-            <img src={props.post.image} alt='Other posts for this user' style={{'width': '100%'}}/>
-        </a>
+        <div className='other-post'>
+            <a href={imgSrcLink}>
+                <img src={props.post.image} alt='Other posts for this user' className='other-post__image' />
+            </a>
+        </div>
     )
 }
 

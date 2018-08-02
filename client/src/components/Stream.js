de25fe7f5349592623fb2f510c7336df021842c3
treykris <trey10027@gmail.com>
2018-08-01 20:49:35 -0400

    Added react-stack-grid to get pinterest effect on stream items

--- client/src/components/Stream.js
+++ client/src/components/Stream.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import StreamItem from './StreamItem';
+import StackGrid from "react-stack-grid";
+import './Stream.css';
 
 
 const Stream = (props) => {
 
+    const windowWidth = window.outerWidth;
     let listPosts;
     let postData;
+    let colWidth;
+
+    if(windowWidth <= 600) colWidth = '45%';
+    if(windowWidth >= 600) colWidth = '30%';
+    
+
 
     // Boolean to check if we should add a username header to our stream of posts
     const sourceFetch = props.sourceFetch === 'stream';
@@ -19,13 +28,14 @@ const Stream = (props) => {
             return <StreamItem post={post} usernameHeader={sourceFetch} key={post._id} edit={props.edit} handlePostDelete={props.handlePostDelete}/>
         })
     }
+    
 
     return(
-        <div>
-            <div className='testMore'>
+        <section className='stream'>
+            <StackGrid columnWidth={colWidth} monitorImagesLoaded={true} gutterWidth={10} gutterHeight={5}>
                 {listPosts}
-            </div>
-        </div>
+            </StackGrid>
+        </section>
     )
 }
 

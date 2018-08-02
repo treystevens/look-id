ee49173cfb6976823f128c8ae05c8dc85c78e452
treykris <trey10027@gmail.com>
2018-08-01 20:56:35 -0400

    added classes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import AccountHead from './AccountHead';
+import AccountNav from './AccountNav';
 import { NavLink, Link } from 'react-router-dom';
+import './Header.css';
 
 
 class Header extends Component{
 
     render(){
         return(
-            <header>
-                <div>
-                        <Link to='/'><h1>LookID.</h1></Link>
-                    <nav>
-                        <ul>
-                            <li><NavLink to='/'>Home</NavLink></li>
-                            <li><NavLink to='/explore'>Explore</NavLink></li>
-                            <li><NavLink to='/feed'>Feed</NavLink></li>
-                            <li><NavLink to='/boards'>Boards</NavLink></li>
+            <header className='container  header-container--flex'>
+                <div className='header-site-nav'>
+                    <Link to='/'><h1 className='logo'>LookID.</h1></Link>
+                    <nav className='global-nav'>
+                        <ul className='site-nav-list'>
+                            <li className='site-nav-list__item'><NavLink to='/'>Home</NavLink></li>
+                            <li className='site-nav-list__item'><NavLink to='/feed'>Feed</NavLink></li>
+                            <li className='site-nav-list__item'><NavLink to='/boards'>Boards</NavLink></li>
                         </ul>
                     </nav>
                 </div>
-                <AccountHead/>
+                <AccountNav/>
             </header>
         )
     }

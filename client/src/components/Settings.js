39b6b2036542763f8b6148225df5ae779e1f6a46
treykris <trey10027@gmail.com>
2018-08-01 20:52:25 -0400

    Added classes, nested Link elems

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import PageHead from './PageHead';
 import { Link } from 'react-router-dom';
+import './Settings.css';
 
 const Settings = (props) => {
     return(
-        <section >
+        <section className='container'>
             <PageHead pageHead='Settings' />
-            <Link to="/profile/settings/change-password" style={{display: 'block'}}>Change Password</Link>
-            <Link to="/profile/settings/delete" style={{display: 'block'}}>Delete Account</Link>
+            <div className='settings-container'>
+                <Link to="/profile/settings/change-password" className='profile__setting'>Change Password</Link>
+                <Link to="/profile/settings/delete" className='profile__setting' >Delete Account</Link>
+            </div>
         </section>
     )
 }

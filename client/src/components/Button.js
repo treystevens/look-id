ac4218e34af785c2e1e8885d0c0e4c46381207cf
treykris <trey10027@gmail.com>
2018-08-01 20:37:49 -0400

    created Button component

---
+++ client/src/components/Button.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Button.css';
+import InputField from './InputField';
+
+const Button = (props) => {
+
+    let btnClass = 'btn';
+    if(props.addClass) btnClass += ` ${props.addClass}`;
+
+    if(props.dummy){
+        return <button type='button' className={btnClass} onClick={props.onClick}>{props.text}</button> 
+    }
+
+    return(
+        <button className={btnClass}>{props.text}</button>
+    )
+};
+
+export default Button;

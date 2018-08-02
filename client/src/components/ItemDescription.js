edd614cd334dd17dba997c3138c2cc201f27fc45
treykris <trey10027@gmail.com>
2018-08-01 20:56:23 -0400

    added classes

--- client/src/components/ItemDescription.js
+++ client/src/components/ItemDescription.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { prefixURL } from '../util/general';
+import './PostItems.css';
 
 
 
@@ -7,34 +8,40 @@ import { prefixURL } from '../util/general';
 const ItemDescription = (props) => {
 
     const itemLink = prefixURL(props.item.link);
+    let storeLength = props.item.stores.length;
 
     // Map out the stores ( they are passed as an array of stores )
     const stores = props.item.stores.map((store, index) => {
         
-        return <div key={`${props.itemNumber}-store${index}`}>{store}</div>
+        let displayStore = `${store},`;
+        if(index === storeLength - 1) displayStore = store;
+
+
+        return <div key={`${props.itemNumber}-store${index}`} className='store'>{displayStore}</div>
     }); 
 
 
     return(
-        <article style={{display: 'flex', flexDirection: 'column'}}>
-            <h3>{props.item.name}</h3>
+        <article className='pi-item'>
+            <span className='pi-item__name'>{props.item.name}</span>
 
             {props.item.price &&
-                <span>${props.item.price}</span>
+                <span className='pi-item__price'>${props.item.price}</span>
             }
 
             {props.item.thrifted && 
                 <span>Thrifted</span>}
 
-            <div style={{display: 'flex'}}>
-
-                {itemLink && 
-                    <a href={itemLink} target='_blank'>Link to Item</a>
-                }
+            <div className='pi-item__stores'>
                 
                 {stores}
-       
             </div>
+                {itemLink && 
+                <div>
+                    <a href={itemLink} target='_blank' className='pi-item__link'>Link to Item</a>
+                </div>
+                }
+            
         </article>
     )
 }

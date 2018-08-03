2a71ccce3b8fdd1300b2c4ab59c3e267efae90dc
treykris <trey10027@gmail.com>
2018-08-03 16:51:33 -0400

    clean up double quotes to single

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -45,14 +45,14 @@ class Search extends Component{
         
         return(
             <article className='search-container'>
-                <form action='/search' className='form' autoComplete='off'>
+                <form action='/search' className='search' autoComplete='off'>
                     <div className='form__search-container'>
                         <InputField type='text' name='query' placeholder='Search clothes, brands, etc.' onChange={this.handleQueryChange} search={true}required={true}/>
                             <button className='form__search-btn'>
-                                <svg xmlns="http://www.w3.org/2000/svg" className='form__search-icon' width="33" height="33" viewBox="0 0 33 33">
-                                    <g fill="none" fillRule="evenodd" stroke="#979797" transform="translate(2 2)">
-                                    <path strokeLinecap="square" strokeWidth="3" d="M18.7541631,18.7373487 L28.5389438,28.5133567"/>
-                                    <ellipse cx="10.6" cy="10.591" strokeWidth="3" rx="10.6" ry="10.591"/>
+                                <svg xmlns='http://www.w3.org/2000/svg' className='form__search-icon' width='20' height='30' viewBox='0 0 33 33'>
+                                    <g fill='none' fillRule='evenodd' stroke='#979797' transform='translate(2 2)'>
+                                    <path strokeLinecap='square' strokeWidth='3' d='M18.7541631,18.7373487 L28.5389438,28.5133567'/>
+                                    <ellipse cx='10.6' cy='10.591' strokeWidth='3' rx='10.6' ry='10.591'/>
                                     </g>
                                 </svg>
                             </button>

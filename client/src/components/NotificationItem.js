bb49da5ee2c580f992ec030140c1a30eba4adebc
treykris <trey10027@gmail.com>
2018-08-02 23:53:50 -0400

    Added classes / styling

--- client/src/components/NotificationItem.js
+++ client/src/components/NotificationItem.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Avatar from './Avatar';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import './Notifications.css';
 
 
 const NotificationItem = (props) => {
 
 
-    console.log(props)
     const { action, _user:user, _post: post, comment } = props.notification;
     let displayText;
     let hasPost = true;
@@ -17,8 +17,8 @@ const NotificationItem = (props) => {
     if(!post) hasPost = false;
     if(!comment) hasComment = false;
     if(action === 'WELCOME') return (
-        <div>
-                <p>{comment}</p>
+        <div className='notification-container'>
+                <h3>{comment}</h3>
         </div>
     )
 
@@ -39,23 +39,30 @@ const NotificationItem = (props) => {
 
 
     return(
-        <div style={{display: 'flex', alignItems: 'center'}}>
-            <Link to={`/user/${user.username}`}>
-                <Avatar avatar={user.profile.avatar} username={user.username}/>
-            </Link>
-            <div>
-                <p>{displayText}</p>
-                {hasComment &&
-                <p>{comment}</p>}
+        <div className='notification-container'>
+            <div className='notification__user'>
+                <Link to={`/user/${user.username}`}>
+                    <Avatar avatar={user.profile.avatar} username={user.username} addClass='avatar-container--small'/>
+                </Link>
+                <div className='notification__message'>
+                    <p>{displayText}</p>
+
+                    {hasComment &&
+                        <p>{comment}</p>
+                    }
+                </div>
             </div>
 
 
                 {hasPost && 
-                    <div style={{width: '80px', height: '80px'}}>
-                        <Link to={`/user/${props.username}/${post.post_id}`}>
-                            <img src={`${post.image}`} alt={`${props.username}'s post.`}  style={{width: '100%'}}/>
+                    
+                        <Link to={`/user/${props.username}/${post.post_id}`} className='notification-container__image'>
+                            <div className='notification__image' style={ { backgroundImage:`url(${post.image}`} }>
+                            </div>
+
+                        
                         </Link>
-                    </div>
+                    
                 }
         </div>
     )

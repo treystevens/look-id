f083656ed6374464f260747cc701e6e9bf90528e
treykris <trey10027@gmail.com>
2018-08-04 10:33:43 -0400

    added heroku env for cloudinary

--- server.js
+++ server.js
@@ -34,12 +34,14 @@ const sessionSecret = process.env.HEROKU_SESSION_SECRET || process.env.LOCAL_SES
 const app = express();
 
 const cloudinary = require('cloudinary');
-
+const cloudName = process.env.HEROKU_CLOUD_NAME || process.env.CLOUDINARY_CLOUD_NAME;
+const cloudKey = process.env.HEROKU_CLOUD_KEY || process.env.CLOUDINARY_API_KEY;
+const cloudSecret = process.env.HEROKU_CLOUD_SECRET || process.env.CLOUDINARY_API_SECRET;
 
 cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    cloud_name: cloudName, 
+    api_key: cloudKey,
+    api_secret: cloudSecret
 });
 
 

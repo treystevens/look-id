ed9e98796c773a1df5a284cc0d650f86c5b5f0a9
treykris <trey10027@gmail.com>
2018-08-04 03:45:08 -0400

    added path module

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const passportSetup = require('./config/passport-setup');
 

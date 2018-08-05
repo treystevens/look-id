c17a1cebd6dd1be1dba04ff907925a9823425f25
treykris <trey10027@gmail.com>
2018-08-05 11:40:08 -0400

    changed anchor tags to Link from react router

--- client/src/components/OtherPosts.js
+++ client/src/components/OtherPosts.js
@@ -1,15 +1,15 @@
 import React from 'react';
-
-
+import { Link } from 'react-router-dom';
+ 
 const OtherPosts = (props) => {
     
     const imgSrcLink = `/user/${props.username}/${props.post.post_id}`;
     
     return(
-        <div className='other-post'>
-            <a href={imgSrcLink}>
+        <div className='other-post' key={props.post.post_id}>
+            <Link to={imgSrcLink}>
                 <img src={props.post.image} alt='Other posts for this user' className='other-post__image' />
-            </a>
+            </Link>
         </div>
     )
 }
